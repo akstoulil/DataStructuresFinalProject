3d9658ea50980913ad9dc728b6db45e81e443a9d
akstoulil <32655655+akstoulil@users.noreply.github.com>
2017-11-03 17:26:41 -0500

    Making a couple of changes to the Employee and CompanyList classes

--- src/CompanyList.java
+++ src/CompanyList.java
@@ -1,4 +1,21 @@
 
 public class CompanyList {
+	
+	private Employee first;
+	private double companyBalance;
+	
+	public CompanyList(){
+		
+		first = null;
+		companyBalance = 1000000;
+		
+	}
+	
+	public CompanyList(double companyBalance){
+		
+		first = null;
+		this.companyBalance = companyBalance;
+		
+	}
 
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,93 @@
 
 public class Employee {
+	
+	private int key;
+	private String firstName;
+	private String lastName;
+	private double weeklyPay;
+	private double balance;
+	private Employee next;
+
+	public Employee(){
+		
+	}
+	
+	public Employee(int key, String firstName, String lastName, double weeklyPay, double balance){
+		
+		this.key = key;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.weeklyPay = weeklyPay;
+		this.balance = balance;
+		
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getWeeklyPay() {
+		return weeklyPay;
+	}
+
+	public void setWeeklyPay(double weeklyPay) {
+		this.weeklyPay = weeklyPay;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	public Employee getNext() {
+		return next;
+	}
+
+	public void setNext(Employee next) {
+		this.next = next;
+	}
+	
+	
+	public void addFunds(double amount){
+		
+		balance += amount;
+		
+	}
+	
+	public void withdrawExternalFunds(double amount){
+		
+		balance -= amount;
+		
+	}
+	
+	public void displayInfo(){
+		
+		System.out.println("Employee Key: " + getKey() + " || First Name: " + getFirstName() + " || Last Name: " + getLastName()
+		+ "\nWeekly Pay: " + getWeeklyPay() + " || Current Balance: " + getBalance());
+		
+	}
 
 }
